3a3f72864c1a6936d1d6c847c443b601f9dcb1fa
Rohit kumar <rohitkumar65108@gmail.com>
2023-05-01 23:42:57 +0530

    Day2

---
+++ Day2/MeanAndMedianofArray.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    //Function to find median of the array elements.
+    int median(int A[],int N)
+    {
+        
+        sort(A,A+N);
+        
+        //your code here
+        //If median is in fraction then convert it to integer and return
+        if(N%2==1)
+        {
+            return A[N/2];
+        }
+        int first=(N/2)-1;
+        int second=(N/2);
+        
+        return floor((A[first]+A[second])/2);
+    }
+    //Function to find mean of the array elements.
+    int mean(int A[],int N)
+    {
+        //your code here
+        int temp=0;
+        for(int i=0;i<N;i++)
+        {
+            temp+=A[i];
+        }
+        
+        return floor(temp/N);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    //testcase
+    int T;
+    cin>>T;
+   
+    //looping testcase
+    while(T--)
+    {
+        //number of elements in array
+        int N;
+        cin>>N;
+        int a[N];
+        
+        //inseting elements in the array
+        for(int i=0;i<N;i++){
+            cin>>a[i];
+        }
+        Solution ob;
+        //calling mean() and median() functions
+        cout<<ob.mean(a,N)<<" "<<ob.median(a,N)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
