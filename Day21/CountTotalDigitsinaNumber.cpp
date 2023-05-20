943c36439ce3424572a47d8a7d860c952358d0f8
Rohit kumar <rohitkumar65108@gmail.com>
2023-05-21 00:04:26 +0530

    Day21

---
+++ Day21/CountTotalDigitsinaNumber.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    //Complete this function
+    int countDigits(int n)
+    {
+        //if there is no digit left return 0
+       if(n==0)
+       {
+           return 0;
+       }
+       //add count 1 for last digit and call for other digits
+       return 1+countDigits(n/10);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int T;
+	
+	//taking testcases
+	cin>>T;
+	while(T--)
+	{
+	    int n;
+	    
+	    //taking number n
+	    cin>>n;
+	    
+	    //calling countDigits
+	    Solution obj;
+	    cout<<obj.countDigits(n)<<endl;
+	    
+	    
+	}
+	return 0;
+}
+
+
+
+// } Driver Code Ends
